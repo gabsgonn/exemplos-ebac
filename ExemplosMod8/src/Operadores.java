f1b91a1c965e41ad886690797d2075d33c557bb4
gabsgonn <65032874+gabsgonn@users.noreply.github.com>
2024-11-14 15:16:33 +0000

    add tasks

--- ExemplosMod8/src/Operadores.java
+++ ExemplosMod8/src/Operadores.java
@@ -1,6 +1,10 @@
-public class OperadoresAritimeticos {
+public class Operadores {
 
     public static void main(String[] args) {
+        operadoresAritimeticos();
+    };
+
+    public static void operadoresAritimeticos() {
         int num1 = 10;
         int num2 = 20;
 
@@ -8,11 +12,12 @@ public class OperadoresAritimeticos {
         int num4 = num1 - num2;
         int num5 = num1 / num2;
         int num6 = num1 * num2;
-
+        int num7 = (87 + 15) * 20;
 
         System.out.println(num3);
         System.out.println(num4);
         System.out.println(num5);
         System.out.println(num6);
-    }
-}
+        System.out.println(num7);
+    };
+};
