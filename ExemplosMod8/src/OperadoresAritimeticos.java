d8e6fc05d5a1b4dd4c981f0d1868bbde9d279b54
gabsgonn <gab.gd23@gmail.com>
2024-11-14 11:47:52 -0300

    add `tasks`

---
+++ ExemplosMod8/src/OperadoresAritimeticos.java
@@ -0,0 +1,18 @@
+public class OperadoresAritimeticos {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int num3 = num1 + num2;
+        int num4 = num1 - num2;
+        int num5 = num1 / num2;
+        int num6 = num1 * num2;
+
+
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+    }
+}
